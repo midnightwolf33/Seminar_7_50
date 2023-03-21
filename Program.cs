ab6e4d8851f0a231d18fb5b9720b3bd3298ce4ea
midnightwolf33 <125289934+midnightwolf33@users.noreply.github.com>
2023-03-21 20:10:38 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 50. Напишите программу, которая на вход принимает 
+// значение элемента в двумерном массиве, и возвращает позицию 
+// этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+int GetNumber(string messege)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.Write(messege);
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else Console.WriteLine("Не корректный ввод числа");
+    }
+    return result;
+}
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 20);
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+
+void FindNumber(int[,] matrix, int number)
+{
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] == number)
+            {
+                Console.WriteLine($"Число {number} найден на позиции {i+1} {j+1}");
+                count++;
+            }
+        }
+    }
+    if (count == 0) Console.WriteLine(number + "-> такого числа в массиве нет");
+}
+
+int rows = GetNumber("Введите количество строк -> ");
+int columns = GetNumber("Введите количество столбцов -> ");
+int number = GetNumber("Введите число для поиска -> ");
+int[,] matrix = InitMatrix(rows, columns);
+
+Console.WriteLine();
+FindNumber(matrix, number);
